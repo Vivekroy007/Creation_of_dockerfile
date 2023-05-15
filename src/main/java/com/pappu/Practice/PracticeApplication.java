ae42fcd31c6d6cd3968b5f0364eb750b52b2d7cc
Vivekroy007 <132645277+Vivekroy007@users.noreply.github.com>
2023-05-15 12:15:17 +0530

    Update PracticeApplication.java

--- src/main/java/com/pappu/Practice/PracticeApplication.java
+++ src/main/java/com/pappu/Practice/PracticeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PracticeApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hello this is my first docker file");
 		SpringApplication.run(PracticeApplication.class, args);
 	}
 
